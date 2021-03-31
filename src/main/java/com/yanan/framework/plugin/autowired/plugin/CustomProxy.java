29608fa34305afee4edc360a57d113d34272814c
tja414312570 <414312570@qq.com>
2021-03-31 11:16:06 +0800

    增加注入栈功能，用于提供一些实例类需要判断依赖来源的功能

--- src/main/java/com/yanan/framework/plugin/autowired/plugin/CustomProxy.java
+++ src/main/java/com/yanan/framework/plugin/autowired/plugin/CustomProxy.java
@@ -1,6 +1,11 @@
 package com.yanan.framework.plugin.autowired.plugin;
 
+/**
+ * 自定义bean接口
+ * 提供用户自定义用户需要的类型bean的注册
+ * @author yanan
+ * @param <T>
+ */
 public interface CustomProxy<T> {
-	
 	T getInstance();
 }
---
+++ src/main/java/com/yanan/framework/plugin/autowired/plugin/WiredStackContext.java
@@ -0,0 +1,134 @@
+package com.yanan.framework.plugin.autowired.plugin;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Stack;
+
+import com.yanan.framework.plugin.definition.RegisterDefinition;
+import com.yanan.framework.plugin.handler.MethodHandler;
+
+/**
+ * 调用栈记录，用于记录当前栈的数据
+ * 当前被注入实例仅能感知到上一个依赖类的数据
+ * @author yanan
+ *
+ */
+public class WiredStackContext {
+	public static final int FIELD = 0;
+	public static final int METHOD = 1;
+	public static final int CONSTRUCT = 2;
+	private static InheritableThreadLocal<Stack<Object[]>> currentContext = new InheritableThreadLocal<>();
+	public static void push(Object... datas) {
+		Stack<Object[]> stack = currentContext.get();
+		if(stack == null) {
+			stack = new Stack<>();
+			currentContext.set(stack);
+		}
+		stack.push(datas);
+	}
+	public static int stackType() {
+		Object[] stacks = peek();
+		if(stacks == null ) 
+			return -1;
+		return (int)stacks[0];
+	}
+	public static String stackString() {
+		int stackType = stackType();
+		switch(stackType) {
+			case 0:
+				return "Field:"+getField();
+			case 1:
+				return "Method"+getMethod();
+			case 2:
+				return "Construector"+getConstructor();
+			default:
+				return "Unknow";
+		}
+	}
+	public static void pop() {
+		Stack<Object[]> stack = currentContext.get();
+		if(stack != null) 
+			stack.pop();
+	}
+	public static Object[] peek() {
+		Stack<Object[]> stack = currentContext.get();
+		if(stack != null && stack.peek().length > 0) 
+			return stack.peek();
+		return null;
+	}
+	public static MethodHandler getMethodHandler() {
+		Object[] stacks = peek();
+		if(stacks != null && (int)stacks[0] == METHOD) {
+			return (MethodHandler) stacks[1];
+		}
+		return null ; 
+	}
+	public static RegisterDefinition getRegisterDefintion() {
+		Object[] stacks = peek();
+		if(stacks == null)
+			return null;
+		if((int)stacks[0] == METHOD) {
+			return ((MethodHandler) stacks[1]).getPlugsProxy().getRegisterDefinition();
+		}
+		if((int)stacks[0] == CONSTRUCT) {
+			return (RegisterDefinition) stacks[1];
+		}
+		if((int)stacks[0] == FIELD) {
+			return (RegisterDefinition) stacks[1];
+		}
+		return null ; 
+	}
+	@SuppressWarnings("unchecked")
+	public static <T> T getProxyInstance() {
+		Object[] stacks = peek();
+		if(stacks == null)
+			return null;
+		if((int)stacks[0] == METHOD) {
+			return ((MethodHandler) stacks[1]).getPlugsProxy().getProxyObject();
+		}
+//		if((int)stacks[0] == CONSTRUCT) {
+//			return (RegisterDefinition) stacks[1];
+//		}
+		if((int)stacks[0] == FIELD) {
+			return (T) stacks[2];
+		}
+		return null ; 
+	}
+	public static Method getMethod() {
+		Object[] stacks = peek();
+		if(stacks != null && (int)stacks[0] == METHOD) {
+			return ((MethodHandler) stacks[1]).getMethod();
+		}
+		return null;
+	}
+	public static Constructor<?> getConstructor() {
+		Object[] stacks = peek();
+		if(stacks != null && (int)stacks[0] == CONSTRUCT) {
+			return (Constructor<?>) stacks[3];
+		}
+		return null;
+	}
+	public static Field getField() {
+		Object[] stacks = peek();
+		if(stacks != null && (int)stacks[0] == FIELD) {
+			return (Field) stacks[5];
+		}
+		return null;
+	}
+	public static Object[] getParameters() {
+		Object[] stacks = peek();
+		if(stacks == null)
+			return null;
+		if((int)stacks[0] == METHOD) {
+			return ((MethodHandler) stacks[1]).getParameters();
+		}
+		if((int)stacks[0] == CONSTRUCT) {
+			return (Object[]) stacks[4];
+		}
+//		if((int)stacks[0] == FIELD) {
+//			return (T) stacks[2];
+//		}
+		return null ; 
+	}
+}
